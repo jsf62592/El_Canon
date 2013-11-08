ba95ae63b95bf3f38f32be5dc23ebcece436ca06
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2013-11-08 14:32:17 -0500

    Initial Commit of unzipped Unity3D project
    Attempting to commit with Unity enabled for source control

---
+++ Assets/Standard Assets/Scripts/BlindEnemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Handle basic AI for blind enemies
+public class BlindEnemy : MonoBehaviour {
+	// Get the player character to access noise level variable
+	public GameObject playerCharacter;
+	public PlayerNoise playerNoise;
+	
+	// Use this for initialization
+	void Start () {
+		playerCharacter = GameObject.Find("Player");
+		PlayerNoise playerNoise = playerCharacter.GetComponent<PlayerNoise>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	// Calculate the distance between this enemy and the player
+	Vector3 DistToPlayer(){
+		return gameObject.transform.position - playerCharacter.transform.position;
+		
+	}
+	
+	// Calculate when the enemy should detect player based on noise
+	void NoiseDetection(){
+		// If the enemy is less than 10 units away from player in any direction
+		if (DistToPlayer().x < 10 || DistToPlayer().y < 10 || DistToPlayer().z < 10){
+			// If the noise level is over 20, trigger basic awareness of enemy
+			if (playerNoise.noiseLevel > 20){
+				// Invoke basic pathing
+				
+			// If the noise level is over 40, trigger seeking for player
+			}else if(playerNoise.noiseLevel > 40){
+				// Invoke seeking
+			
+			// If the noise level is over 70, enemy will know where player is
+			}else if(playerNoise.noiseLevel > 70){
+				// Invoke direct path to player
+			}
+		}
+	}
+}
---
+++ Assets/Standard Assets/Scripts/DeafEnemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Handle basic AI for deaf enemies
+public class DeafEnemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
